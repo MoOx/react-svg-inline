c7fa22604e46965fa0f5fab548b31f95d8ca4307
Maxime Thirouin <m@moox.io>
2018-06-07 19:39:31 +0200

    format

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,199 +1,196 @@
 /* eslint-disable react/no-multi-comp, react/prop-types, react/jsx-no-bind */
 
-import test from "ava"
-import React from "react"
-import ReactDOMServer from "react-dom/server"
+import test from "ava";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
 
-import SVGInline from ".."
+import SVGInline from "..";
 
 // const result = ?
 // => https://github.com/sindresorhus/ava/issues/274
 
-test("SVGInline: passes & merges className", (t) => {
+test("SVGInline: passes & merges className", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
     <SVGInline className="TestSVG" svg="<svg><g></g></svg>" />
-  )
+  );
   t.is(
     result,
-    "<span class=\"SVGInline TestSVG\"><svg class=\"SVGInline-svg "+
-      "TestSVG-svg\"" +
-    "><g></g></svg></span>"
-  )
-})
+    '<span class="SVGInline TestSVG"><svg class="SVGInline-svg ' +
+      'TestSVG-svg"' +
+      "><g></g></svg></span>"
+  );
+});
 
-test("SVGInline: parent component can be chosen by tagName", (t) => {
+test("SVGInline: parent component can be chosen by tagName", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline
-      component="div"
-      className="TestSVG"
-      svg="<svg><g></g></svg>"
-    />
-  )
+    <SVGInline component="div" className="TestSVG" svg="<svg><g></g></svg>" />
+  );
   t.is(
     result,
-    "<div class=\"SVGInline TestSVG\"><svg class=\"SVGInline-svg TestSVG-svg\""+
+    '<div class="SVGInline TestSVG"><svg class="SVGInline-svg TestSVG-svg"' +
       "><g></g></svg></div>"
-  )
-})
+  );
+});
 
-test("SVGInline: parent composite component can be chosen", (t) => {
-  const TestComponent = (props) => {
-    return (
-      <div {...props} className="foo" />
-    )
-  }
+test("SVGInline: parent composite component can be chosen", t => {
+  const TestComponent = props => {
+    return <div {...props} className="foo" />;
+  };
 
   const result = ReactDOMServer.renderToStaticMarkup(
     <SVGInline
-      component={ TestComponent }
+      component={TestComponent}
       className="TestSVG"
       svg="<svg><g></g></svg>"
     />
-  )
+  );
   t.is(
     result,
-    "<div class=\"foo\"><svg class=\"SVGInline-svg TestSVG-svg\"><g></g></svg>"+
+    '<div class="foo"><svg class="SVGInline-svg TestSVG-svg"><g></g></svg>' +
       "</div>"
-  )
-})
+  );
+});
 
-const svgPiece = "width=\"24\" height=\"16px\"><g fill=\"none\"><path " +
-  "fill=\"#ab234f\"></path></g></svg>"
-const SVGInlineStart = "<span class=\"SVGInline\"><svg class=\"SVGInline-svg\""
-const SVGInlineCleanedStart = "<span class=\"SVGInline SVGInline--cleaned\">" +
-  "<svg class=\"SVGInline-svg SVGInline--cleaned-svg\""
+const svgPiece =
+  'width="24" height="16px"><g fill="none"><path ' +
+  'fill="#ab234f"></path></g></svg>';
+const SVGInlineStart = '<span class="SVGInline"><svg class="SVGInline-svg"';
+const SVGInlineCleanedStart =
+  '<span class="SVGInline SVGInline--cleaned">' +
+  '<svg class="SVGInline-svg SVGInline--cleaned-svg"';
 
-test("SVGInline: doesn't cleanup the svg by default", (t) => {
-
+test("SVGInline: doesn't cleanup the svg by default", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ `<svg ${ svgPiece }` } />
-  )
+    <SVGInline svg={`<svg ${svgPiece}`} />
+  );
+  t.is(result, `${SVGInlineStart} ${svgPiece}</span>`);
+});
+
+test("SVGInline: can cleanup the svg", t => {
+  const result = ReactDOMServer.renderToStaticMarkup(
+    <SVGInline cleanup svg={`<svg ${svgPiece}`} />
+  );
+  t.is(result, `${SVGInlineCleanedStart}><g><path></path></g></svg></span>`);
+});
+
+test("SVGInline: cleanup the svg with exceptions", t => {
+  const result = ReactDOMServer.renderToStaticMarkup(
+    <SVGInline cleanupExceptions={["fill"]} svg={`<svg ${svgPiece}`} />
+  );
   t.is(
     result,
-    `${ SVGInlineStart } ${ svgPiece }</span>`
-  )
-})
-
-test("SVGInline: can cleanup the svg", (t) => {
-  const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline cleanup svg={ `<svg ${ svgPiece }` } />
-  )
-  t.is(
-    result,
-    `${ SVGInlineCleanedStart }><g><path></path></g></svg></span>`
-  )
-})
-
-test("SVGInline: cleanup the svg with exceptions", (t) => {
-  const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline cleanupExceptions={ [ "fill" ] } svg={ `<svg ${ svgPiece }` } />
-  )
-  t.is(
-    result,
-    `${ SVGInlineCleanedStart }><g fill="none"><path fill="#ab234f"></path>` +
+    `${SVGInlineCleanedStart}><g fill="none"><path fill="#ab234f"></path>` +
       "</g></svg></span>"
-  )
-})
+  );
+});
 
-test("SVGInline: should add width (and height automatically)", (t) => {
+test("SVGInline: should add width (and height automatically)", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } width="1rem" />
-  )
+    <SVGInline svg={"<svg><g></g></svg>"} width="1rem" />
+  );
   t.is(
     result,
-    `${ SVGInlineStart } style="width: 1rem;height: 1rem;"><g></g></svg></span>`
-  )
-})
+    `${SVGInlineStart} style="width: 1rem;height: 1rem;"><g></g></svg></span>`
+  );
+});
 
-test("SVGInline: should add width & height", (t) => {
+test("SVGInline: should add width & height", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } width="1rem" height="auto" />
-  )
+    <SVGInline svg={"<svg><g></g></svg>"} width="1rem" height="auto" />
+  );
   t.is(
     result,
-    `${ SVGInlineStart } style="width: 1rem;height: auto;"><g></g></svg></span>`
-  )
-})
+    `${SVGInlineStart} style="width: 1rem;height: auto;"><g></g></svg></span>`
+  );
+});
 
-test("SVGInline: should add height", (t) => {
+test("SVGInline: should add height", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } height="1rem" />
-  )
-  t.is(
-    result,
-    `${ SVGInlineStart } style="height: 1rem;"><g></g></svg></span>`
-  )
-})
+    <SVGInline svg={"<svg><g></g></svg>"} height="1rem" />
+  );
+  t.is(result, `${SVGInlineStart} style="height: 1rem;"><g></g></svg></span>`);
+});
 
-test("SVGInline: does not pass internal props to component", (t) => {
-  const TestComponent = (props) => {
-    t.not(props.className, undefined)
-    t.is(props.component, undefined)
-    t.is(props.classSuffix, undefined)
-    t.is(props.fill, undefined)
-    t.is(props.cleanup, undefined)
-    t.is(props.cleanupExceptions, undefined)
+test("SVGInline: does not pass internal props to component", t => {
+  const TestComponent = props => {
+    t.not(props.className, undefined);
+    t.is(props.component, undefined);
+    t.is(props.classSuffix, undefined);
+    t.is(props.fill, undefined);
+    t.is(props.cleanup, undefined);
+    t.is(props.cleanupExceptions, undefined);
 
-    return <div className="foo" />
-  }
+    return <div className="foo" />;
+  };
 
   ReactDOMServer.renderToStaticMarkup(
     <SVGInline
-      component={ TestComponent }
+      component={TestComponent}
       className="TestSVG"
       classSuffix="-test"
       fill="#000000"
       svg="<svg><g></g></svg>"
-      cleanupExceptions={ [ "fake" ] }
+      cleanupExceptions={["fake"]}
       cleanup
     />
-  )
+  );
+});
 
-})
-
-test("SVGInline: includes title element if accessibilityLabel is provided", (t) => {
+test("SVGInline: includes title element if accessibilityLabel is provided", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } accessibilityLabel="My test title" />
-  )
+    <SVGInline svg={"<svg><g></g></svg>"} accessibilityLabel="My test title" />
+  );
   t.is(
     result,
-    `${ SVGInlineStart } role="img" aria-labelledby="SVGInline-0-title"><title id="SVGInline-0-title">My test title</title><g></g></svg></span>`
-  )
-})
+    `${SVGInlineStart} role="img" aria-labelledby="SVGInline-0-title"><title id="SVGInline-0-title">My test title</title><g></g></svg></span>`
+  );
+});
 
-test("SVGInline: accessibilityLabel IDs are not the same", (t) => {
+test("SVGInline: accessibilityLabel IDs are not the same", t => {
   const result1 = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } accessibilityLabel="First test title" />
-  )
+    <SVGInline
+      svg={"<svg><g></g></svg>"}
+      accessibilityLabel="First test title"
+    />
+  );
   const result2 = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } accessibilityLabel="Second test title" />
-  )
+    <SVGInline
+      svg={"<svg><g></g></svg>"}
+      accessibilityLabel="Second test title"
+    />
+  );
   t.is(
     result1,
-    `${ SVGInlineStart } role="img" aria-labelledby="SVGInline-1-title"><title id="SVGInline-1-title">First test title</title><g></g></svg></span>`
-  )
+    `${SVGInlineStart} role="img" aria-labelledby="SVGInline-1-title"><title id="SVGInline-1-title">First test title</title><g></g></svg></span>`
+  );
   t.is(
     result2,
-    `${ SVGInlineStart } role="img" aria-labelledby="SVGInline-2-title"><title id="SVGInline-2-title">Second test title</title><g></g></svg></span>`
-  )
-})
+    `${SVGInlineStart} role="img" aria-labelledby="SVGInline-2-title"><title id="SVGInline-2-title">Second test title</title><g></g></svg></span>`
+  );
+});
 
-test("SVGInline: includes desc element if accessibilityDesc is provided", (t) => {
+test("SVGInline: includes desc element if accessibilityDesc is provided", t => {
   const result = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg><g></g></svg>" } accessibilityDesc="Longer accessibility description of this svg image" />
-  )
+    <SVGInline
+      svg={"<svg><g></g></svg>"}
+      accessibilityDesc="Longer accessibility description of this svg image"
+    />
+  );
   t.is(
     result,
-    `${ SVGInlineStart }><desc>Longer accessibility description of this svg image</desc><g></g></svg></span>`
-  )
-})
+    `${SVGInlineStart}><desc>Longer accessibility description of this svg image</desc><g></g></svg></span>`
+  );
+});
 
-test("SVGInline: accessibilityLabel doesn't crash on svg tag with newlines", (t) => {
+test("SVGInline: accessibilityLabel doesn't crash on svg tag with newlines", t => {
   const result1 = ReactDOMServer.renderToStaticMarkup(
-    <SVGInline svg={ "<svg \n><g></g></svg>" } accessibilityLabel="Third test title" />
-  )
+    <SVGInline
+      svg={"<svg \n><g></g></svg>"}
+      accessibilityLabel="Third test title"
+    />
+  );
   t.is(
     result1,
-    `${ SVGInlineStart } ${'\n'} role="img" aria-labelledby="SVGInline-3-title"><title id="SVGInline-3-title">Third test title</title><g></g></svg></span>`
-  )
-})
+    `${SVGInlineStart} ${"\n"} role="img" aria-labelledby="SVGInline-3-title"><title id="SVGInline-3-title">Third test title</title><g></g></svg></span>`
+  );
+});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import cx from "classnames"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import cx from "classnames";
 
 // import styles from "./styles"
 
@@ -22,8 +22,8 @@ const cleanups = {
   // Sketch.app shit
   sketchMSShapeGroup: / +sketch:type="MSShapeGroup"/gi,
   sketchMSPage: / +sketch:type="MSPage"/gi,
-  sketchMSLayerGroup: / +sketch:type="MSLayerGroup"/gi,
-}
+  sketchMSLayerGroup: / +sketch:type="MSLayerGroup"/gi
+};
 
 // @styled(styles)
 class SVGInline extends Component {
@@ -39,122 +39,97 @@ class SVGInline extends Component {
       classSuffix,
       cleanupExceptions,
       ...componentProps
-    } = this.props
+    } = this.props;
 
-    let {
-      cleanup,
-      height,
-    } = this.props
+    let { cleanup, height } = this.props;
 
     if (
       // simple way to enable entire cleanup
       cleanup === true ||
       // passing cleanupExceptions enable cleanup as well
-      (
-        cleanup.length === 0 &&
-        cleanupExceptions.length > 0
-      )
+      (cleanup.length === 0 && cleanupExceptions.length > 0)
     ) {
-      cleanup = Object.keys(cleanups)
+      cleanup = Object.keys(cleanups);
     }
-    cleanup = cleanup.filter(
-      key => {
-        return !(cleanupExceptions.indexOf(key) > -1)
-      }
-    )
+    cleanup = cleanup.filter(key => {
+      return !(cleanupExceptions.indexOf(key) > -1);
+    });
 
     if (width && height === undefined) {
-      height = width
+      height = width;
     }
 
     // remove useless props for wrapper
-    delete componentProps.cleanup
-    delete componentProps.height
+    delete componentProps.cleanup;
+    delete componentProps.height;
 
     const classes = cx({
-      "SVGInline": true,
+      SVGInline: true,
       "SVGInline--cleaned": cleanup.length,
-      [className]: className,
-    })
-    const svgClasses = classes
-      .split(" ")
-      .join(classSuffix + " ") + classSuffix
+      [className]: className
+    });
+    const svgClasses = classes.split(" ").join(classSuffix + " ") + classSuffix;
     let svgStr = SVGInline.cleanupSvg(svg, cleanup).replace(
       /<svg/,
-      `<svg class="${ svgClasses }"` +
-      (
-        fill
-        ? ` fill="${ fill }"`
-        : ""
-      ) +
-      (
-        width || height
-        ? " style=\"" +
+      `<svg class="${svgClasses}"` +
+        (fill ? ` fill="${fill}"` : "") +
+        (width || height
+          ? ' style="' +
             (width ? `width: ${width};` : "") +
             (height ? `height: ${height};` : "") +
-          "\""
-        : ""
-      )
-    )
-    let match
-    if(accessibilityDesc) {
-      match = /<svg(.|\n|\r\n)*?>/.exec(svgStr)
-      const pos = match.index + match[0].length
-      svgStr = svgStr.substr(0, pos)
-      + `<desc>${accessibilityDesc}</desc>`
-      + svgStr.substr(pos)
+            '"'
+          : "")
+    );
+    let match;
+    if (accessibilityDesc) {
+      match = /<svg(.|\n|\r\n)*?>/.exec(svgStr);
+      const pos = match.index + match[0].length;
+      svgStr =
+        svgStr.substr(0, pos) +
+        `<desc>${accessibilityDesc}</desc>` +
+        svgStr.substr(pos);
     }
-    if(accessibilityLabel) {
-      match = match || /<svg(.|\n|\r\n)*?>/.exec(svgStr)
-      const pos = match.index + match[0].length - 1
-      const id = `SVGInline-${SVGInline.idCount++}-title`
-      svgStr = svgStr.substr(0, pos)
-        + ` role="img" aria-labelledby="${id}"`
-        + svgStr.substr(pos, 1)
-        + `<title id="${id}">${accessibilityLabel}</title>`
-        + svgStr.substr(pos + 1)
+    if (accessibilityLabel) {
+      match = match || /<svg(.|\n|\r\n)*?>/.exec(svgStr);
+      const pos = match.index + match[0].length - 1;
+      const id = `SVGInline-${SVGInline.idCount++}-title`;
+      svgStr =
+        svgStr.substr(0, pos) +
+        ` role="img" aria-labelledby="${id}"` +
+        svgStr.substr(pos, 1) +
+        `<title id="${id}">${accessibilityLabel}</title>` +
+        svgStr.substr(pos + 1);
     }
-    return (
-      React.createElement(
-        component,
-        {
-          ...componentProps, // take most props
-          className: classes,
-          dangerouslySetInnerHTML: {
-            __html: svgStr,
-          },
-        }
-      )
-    )
+    return React.createElement(component, {
+      ...componentProps, // take most props
+      className: classes,
+      dangerouslySetInnerHTML: {
+        __html: svgStr
+      }
+    });
   }
 }
 
 SVGInline.propTypes = {
   className: PropTypes.string,
   classSuffix: PropTypes.string,
-  component: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.func,
-  ]),
+  component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
   svg: PropTypes.string.isRequired,
   fill: PropTypes.string,
-  cleanup: PropTypes.oneOfType([
-    PropTypes.bool,
-    PropTypes.array,
-  ]),
+  cleanup: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
   cleanupExceptions: PropTypes.array,
   width: PropTypes.string,
   height: PropTypes.string,
   accessibilityLabel: PropTypes.string,
-  accessibilityDesc: PropTypes.string,
-}
+  accessibilityDesc: PropTypes.string
+};
 
 SVGInline.defaultProps = {
   component: "span",
   classSuffix: "-svg",
   cleanup: [],
-  cleanupExceptions: [],
-}
+  cleanupExceptions: []
+};
 
 SVGInline.idCount = 0;
 
@@ -162,9 +137,9 @@ SVGInline.cleanupSvg = (svg, cleanup = []) => {
   return Object.keys(cleanups)
     .filter(key => cleanup.indexOf(key) > -1)
     .reduce((acc, key) => {
-      return acc.replace(cleanups[key], "")
+      return acc.replace(cleanups[key], "");
     }, svg)
-    .trim()
-}
+    .trim();
+};
 
-export default SVGInline
+export default SVGInline;
